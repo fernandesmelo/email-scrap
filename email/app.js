96adf2a50fb12f2dbb8b6fc7a841f3b9f3d4893c
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-01 17:57:57 -0300

    refatoração: remove comentários desnecessários e atualiza destinatário e assunto do e-mail

--- email/app.js
+++ email/app.js
@@ -1,26 +1,25 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
-// Criação do transportador de e-mail com base nas variáveis de ambiente
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false, // true para 465, false para outras portas
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-// Função para enviar o e-mail
+/
 async function sendEmail() {
   try {
     const info = await transporter.sendMail({
-      from: `"Nome do Remetente" <${process.env.EMAIL_USER}>`, // Remetente
-      to: 'geraldoj8@gmail.com', // Lista de destinatários
-      subject: 'Assunto do E-mail', // Assunto
-      text: 'Conteúdo do e-mail em texto simples', // Corpo do e-mail em texto simples
-      html: '<b>Conteúdo do e-mail em HTML</b>', // Corpo do e-mail em HTML
+      from: `"Nome do Remetente" <${process.env.EMAIL_USER}>`, 
+      to: 'laercio.neto@edu.pe.senac.br', 
+      subject: 'Notícias - Tecnologia', 
+      text: 'Conteúdo do e-mail em texto simples', 
+      html: '<b>Conteúdo do e-mail em HTML</b>', 
     });
 
     console.log('E-mail enviado: %s', info.messageId);
@@ -29,5 +28,4 @@ async function sendEmail() {
   }
 }
 
-// Dispara o e-mail
 sendEmail();
