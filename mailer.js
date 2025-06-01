d57d9359e7007a1439740680b977d4116c1e3899
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-01 18:37:23 -0300

    feat: refatora mailer.js para usar a sintaxe de importação ES6

--- mailer.js
+++ mailer.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+import 'dotenv/config';
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
